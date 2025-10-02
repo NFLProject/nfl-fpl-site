23e8ff812f4a676b074dd0043ad4a2a8fa0dfb40
NFLProject <marctaylor723@gmail.com>
2025-10-02 13:58:39 +0100

    Create Appclient.tsx

---
+++ app/play/Appclient.tsx
@@ -0,0 +1,11 @@
+// @ts-nocheck
+"use client";
+
+export default function AppClient() {
+  return (
+    <div style={{ padding: 24 }}>
+      <h1>AppClient mounted 🎉</h1>
+      <p>If you can see this at /play, the route compiles.</p>
+    </div>
+  );
+}
